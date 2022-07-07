7e7cfce87d804ecd05c896605d6763a3a91a7224
Richard Hu <r.hu@berkeley.edu>
2022-07-06 21:02:06 -0700

    Use speechcommands dataset in sample_experiment

--- sample_experiment.py
+++ sample_experiment.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 import rayleaf
 from rayleaf.entities import Server, Client
 
@@ -26,29 +29,38 @@ class AmplifyingClient(Client):
         self.train_model()
 
         for param_tensor, layer in self.model_params.items():
-            self.model_params[param_tensor] *= self.amplifying_factor
+            if "num_batches_tracked" not in param_tensor:
+                """
+                M5 model contains BatchNorm layers that have special parameters
+                """
+                self.model_params[param_tensor] *= self.amplifying_factor
+            else:
+                self.model_params[param_tensor] = 0
         
         self.amplifying_factor *= 1.2
 
         return self.num_train_samples, self.model_params
 
 
+curr_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
 rayleaf.run_experiment(
-    dataset = "femnist",
-    dataset_dir = "data/femnist/",
-    output_dir="output/sample/",
-    model = "cnn",
+    dataset = "speech_commands",
+    dataset_dir = "datasets/speech_commands/",
+    output_dir=f"output/sample_experiment-{curr_time}/",
+    model = "m5",
     num_rounds = 20,
-    eval_every = 10,
+    eval_every = 5,
     ServerType=Server,
-    client_types=[(FlippingClient, 2), (AmplifyingClient, 2), (Client, -1)],
-    clients_per_round = 20,
+    client_types=[(FlippingClient, 1), (AmplifyingClient, 1), (Client, 40)],
+    clients_per_round = 5,
     client_lr = 0.06,
     batch_size = 64,
     seed = 0,
     use_val_set = False,
     num_epochs = 5,
-    gpus_per_client_cluster=0.6,
-    num_client_clusters=7,
-    save_model=False
+    gpus_per_client_cluster=0.1,
+    num_client_clusters=2,
+    save_model=False,
+    notes = f"2 malicious clients"
 )
